7630600c82705223a9ca26c623c9a1c4bdbeed43
Leannaaaa <2974963202@qq.com>
2026-02-14 21:52:49 +0000

    y

--- linked list/question.py
+++ linked list/question.py
@@ -1,4 +1,11 @@
 # 206
 # 876  Middle of the linked list 快慢指针
 # 141 Linked List Cycle 快慢指针
-# 142
+# 142 Linked List Cycle || 3指针
+# 19 Remove Nth Node From End of List 快慢指针
+# 21 Merge Two Sorted Lists 
+# 160 Intersection of Two Linked Lists 双指针
+# 234 Palindrome Linked List
+# 2 Add Two Numbers
+#
+#
